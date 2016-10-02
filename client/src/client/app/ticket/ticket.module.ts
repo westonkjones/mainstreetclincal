45230611ca2f38ffe3c1b2577b62f7927e3ed43f
Weston Jones <westonkjones@gmail.com>
2016-10-02 10:04:05 -0500

    Added common module to ticket module

--- client/src/client/app/ticket/ticket.module.ts
+++ client/src/client/app/ticket/ticket.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-import { TicketComponent }   from './index';
-import { TicketCreateComponent }   from './create/ticket.create.component';
+import { TicketComponent } from './index';
+import { TicketCreateComponent } from './create/ticket.create.component';
+import { TicketWidgetComponent } from './widget/ticket.widget.component';
 
 @NgModule({
-    imports: [FormsModule],
-    declarations: [TicketComponent, TicketCreateComponent],
-    exports: [TicketComponent],
+    imports: [FormsModule, CommonModule],
+    declarations: [TicketComponent, TicketCreateComponent, TicketWidgetComponent],
+    exports: [TicketComponent, TicketWidgetComponent],
     providers: [],
 })
 export class TicketModule { }
