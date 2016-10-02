64c83bb7155796277ab0c5d6fbb2d6c03b47632e
Weston Jones <westonkjones@gmail.com>
2016-10-02 11:19:57 -0500

    Added more mock data for the time

--- client/src/client/app/ticket/ticket.service.ts
+++ client/src/client/app/ticket/ticket.service.ts
@@ -8,8 +8,21 @@ export class TicketService {
     constructor() {
         var t1 = new Ticket();
         t1.id = 1;
+        t1.created = '1/1/31';
+        t1.user = 'Wes Jones';
+        t1.creator = 'Jason Wergin';
+        t1.description = 'To bodly go where no one has gone before';
+        t1.nextStop = 'Emma Dean';
+        t1.title = 'Log Stardate';
+
         var t2 = new Ticket();
         t2.id = 2;
+        t2.created = '2/2/31';
+        t2.user = 'Turtle';
+        t2.creator = 'Nip Mouse';
+        t2.description = 'play with nip mouse';
+        t2.nextStop = 'Yertle';
+        t2.title = 'Meow';
 
         this.tickets = [];
         this.tickets.push(t1);
