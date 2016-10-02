15a43e0f9c20e035300547d3f35a09630022294a
Weston Jones <westonkjones@gmail.com>
2016-10-02 10:05:41 -0500

    Added parameter types to service functions

--- client/src/client/app/ticket/ticket.service.ts
+++ client/src/client/app/ticket/ticket.service.ts
@@ -6,17 +6,24 @@ import { Ticket } from './ticket';
 export class TicketService {
     private tickets: Ticket[];
     constructor() {
+        var t1 = new Ticket();
+        t1.id = 1;
+        var t2 = new Ticket();
+        t2.id = 2;
+
         this.tickets = [];
+        this.tickets.push(t1);
+        this.tickets.push(t2);
     }
 
     getTickets(): Ticket[] {
         return this.tickets;
     }
-    addTicket(ticket): boolean {
+    addTicket(ticket: Ticket): boolean {
         this.tickets.push(ticket);
         return true;
     }
-    deleteTicket(ticket): boolean {
+    deleteTicket(ticket: Ticket): boolean {
         var index: number = this.tickets.indexOf(ticket);
         if(index > -1) {
             this.tickets.splice(index);
