2f036024d3c2a87732c0a01bec0127a4661da7c6
Weston Jones <westonkjones@gmail.com>
2016-10-02 10:05:03 -0500

    Ticket component now injects the ticket service from its onInit function

--- client/src/client/app/ticket/ticket.component.ts
+++ client/src/client/app/ticket/ticket.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Ticket } from './ticket';
+import { TicketService } from './ticket.service';
+import { TicketWidgetComponent } from './widget/ticket.widget.component';
 
 @Component({
     moduleId: module.id,
     selector: 'ms-ticket',
     templateUrl: 'ticket.component.html',
-    styleUrls: ['ticket.component.css']
+    styleUrls: ['ticket.component.css'],
+    providers: [TicketService],
+    directives: [TicketWidgetComponent]
 })
-export class TicketComponent { }
+export class TicketComponent implements OnInit {
+    tickets: Ticket[];
+    constructor(private ticketService: TicketService) {
+    }
+    ngOnInit(): void {
+        this.tickets = this.ticketService.getTickets();
+    }
+}
