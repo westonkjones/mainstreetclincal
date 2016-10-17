80e3a81954def6b18a7f03a399a9c184479fe9f4
Weston Jones <westonkjones@gmail.com>
2016-10-16 23:12:00 -0500

    Added user create component

--- client/src/client/app/user/index.ts
+++ client/src/client/app/user/index.ts
@@ -1,1 +1,3 @@
+export * from './user';
 export * from './user.service';
+export * from './create/user.create.component';
--- client/src/client/app/user/user.module.ts
+++ client/src/client/app/user/user.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import { UserService } from './index';
+import { UserService } from './user.service';
+import { UserCreateComponent } from './index';
 
 @NgModule({
     imports: [],
-    exports: [UserService],
-    declarations: [],
+    declarations: [UserCreateComponent],
+    exports: [UserService, UserCreateComponent],
     providers: [UserService],
 })
-export class NameModule { }
+export class NameModule {}
