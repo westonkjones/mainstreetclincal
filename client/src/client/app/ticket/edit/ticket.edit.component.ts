d1807c19fd430d267736cfd29ae5473554f9c0e0
Weston Jones <westonkjones@gmail.com>
2016-10-17 00:58:25 -0500

    Ticket edit now uses autocomplete

--- client/src/client/app/ticket/edit/ticket.edit.component.ts
+++ client/src/client/app/ticket/edit/ticket.edit.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, Output, EventEmitter, AfterViewChecked } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewChecked, OnInit } from '@angular/core';
 
 import { Ticket } from '../ticket';
 import { TicketService } from '../ticket.service';
+import { User } from '../../user/index';
+import { UserService } from '../../user/user.service';
 
 @Component({
     moduleId: module.id,
@@ -9,17 +11,22 @@ import { TicketService } from '../ticket.service';
     templateUrl: 'ticket.edit.component.html',
     styleUrls: ['ticket.edit.component.css']
 })
-export class TicketEditComponent implements AfterViewChecked {
+export class TicketEditComponent implements AfterViewChecked, OnInit {
     @Input() ticket: Ticket;
     @Output() deleteTicketEvent: EventEmitter<any> = new EventEmitter();
+    users: User[];
     private title: string;
     private submitted: boolean;
     private successful: boolean;
     private originalTicket: Ticket;
-    constructor(private ticketService: TicketService) {
+    constructor(private ticketService: TicketService, private userService: UserService) {
+        this.users = [];
         this.title = 'Edit Ticket';
         this.submitted = false;
         this.successful = false;
+    }
+    ngOnInit() {
+        this.userService.getUsers().then(users => this.users = users);
     }
     ngAfterViewChecked() {
         this.originalTicket = this.ticket;
