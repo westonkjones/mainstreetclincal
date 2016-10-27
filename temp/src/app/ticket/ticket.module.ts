92f5280ea383d11baca8060193ec513679438e03
Weston Jones <westonkjones@gmail.com>
2016-10-26 21:12:38 -0500

    Removed temp directory

--- temp/src/app/ticket/ticket.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { TicketComponent } from './ticket.component.ts';
-
-@NgModule({
-    declarations: [TicketComponent],
-    exports: [TicketComponent]
-})
-export class TicketModule { }
