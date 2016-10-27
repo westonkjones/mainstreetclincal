a1fb786cdbde68b631c94e924218cd30ea898370
Weston Jones <westonkjones@gmail.com>
2016-10-26 21:12:38 -0500

    Removed temp directory

--- temp/src/app/ticket/ticket.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { TicketComponent } from './ticket.component.ts';
-
-export const TicketRoutes: Route[] = [
-    {
-        path: 'tickets',
-        component: TicketComponent
-    }
-];
