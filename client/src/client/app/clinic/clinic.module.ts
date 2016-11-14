d612f93732e3b438dd7e6ce6bfa15edf40c9b708
Weston Jones <westonkjones@gmail.com>
2016-11-13 22:18:41 -0600

    Deleting a task now asks for confirmation

--- client/src/client/app/clinic/clinic.module.ts
+++ client/src/client/app/clinic/clinic.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
+import { SharedModule } from '../shared/shared.module';
 
 import { ClinicCreateComponent } from './index'; 
 import { ClinicService } from './clinic.service';
 import { ClinicTaskCreateComponent } from './task/create/clinic.task.create.component';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, Ng2AutoCompleteModule],
+    imports: [CommonModule, FormsModule, Ng2AutoCompleteModule, SharedModule],
     declarations: [ClinicCreateComponent, ClinicTaskCreateComponent],
     exports: [ClinicCreateComponent, ClinicTaskCreateComponent],
     providers: [ClinicService],
