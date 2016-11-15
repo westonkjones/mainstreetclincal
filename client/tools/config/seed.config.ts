cffffc2275ae64a2bdab5173397cf6df71f87f08
Weston Jones <westonkjones@gmail.com>
2016-11-15 01:28:46 -0600

    Fixed app title

--- client/tools/config/seed.config.ts
+++ client/tools/config/seed.config.ts
@@ -160,7 +160,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular2-seed!';
+  APP_TITLE = 'Mainstreet';
 
   /**
    * The base folder of the applications source files.
