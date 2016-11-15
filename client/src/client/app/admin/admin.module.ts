e542f237ca1341972eb94be2ad71062eaeb8bc3a
Weston Jones <westonkjones@gmail.com>
2016-11-14 20:04:35 -0600

    Added ad add clinic button to clinic admin screen

--- client/src/client/app/admin/admin.module.ts
+++ client/src/client/app/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AdminComponent, AdminClinicComponent, ClinicCreateComponent, ClinicTaskCreateComponent }   from './index';
 
 @NgModule({
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, RouterModule],
     exports: [AdminComponent, ClinicCreateComponent],
     declarations: [AdminComponent, AdminClinicComponent, ClinicCreateComponent, ClinicTaskCreateComponent],
     providers: [],
