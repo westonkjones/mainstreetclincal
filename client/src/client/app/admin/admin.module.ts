09deb8d317e514c22d6c5ba509b5da2e84caea3c
Weston Jones <westonkjones@gmail.com>
2016-11-14 21:23:55 -0600

    Added provider for clinic service

--- client/src/client/app/admin/admin.module.ts
+++ client/src/client/app/admin/admin.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AdminComponent, AdminClinicComponent, ClinicCreateComponent, ClinicTaskCreateComponent }   from './index';
+import { ClinicService } from '../clinic/clinic.service';
 
 @NgModule({
     imports: [CommonModule, FormsModule, RouterModule],
     exports: [AdminComponent, ClinicCreateComponent],
     declarations: [AdminComponent, AdminClinicComponent, ClinicCreateComponent, ClinicTaskCreateComponent],
-    providers: [],
+    providers: [ClinicService],
 })
 export class AdminModule { }
